28b47d149054c8b723dea7af81f7c44434880f04
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-30 23:45:49 +0600

    Add ASCII value to character conversion program

---
+++ C Programming/Anisul Islam/Chapter 1/ASCII_val_to_Character.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any ASCII value: ");
+    scanf("%d", &n);
+
+    printf("You entered ASCII Character is: %c\n", n);
+
+    return 0;
+}
