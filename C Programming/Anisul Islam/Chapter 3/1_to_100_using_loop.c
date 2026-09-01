c7d7ae53766195b1800d5c06c158051a14f8ba6d
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-01 23:27:52 +0600

    Add do-while loop to print numbers from 1 to 100
    Implement number printing from 1 to 100 using do-while loop.

---
+++ C Programming/Anisul Islam/Chapter 3/1_to_100_using_loop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    //for loop
+    // for(int i = 1; i <= 100; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    //while loop
+    // int i = 1;
+    // while(i <= 100)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+
+    //do-while loop
+    int i = 1;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    while(i <= 100);
+
+    return 0;
+}
