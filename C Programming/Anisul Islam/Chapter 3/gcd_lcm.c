e61299279bd47ab91de017504cf3e1be5869a274
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-02 23:30:41 +0600

    Add GCD and LCM calculation program in gcd_lcm.c
    This program calculates the GCD and LCM of two numbers entered by the user.

---
+++ C Programming/Anisul Islam/Chapter 3/gcd_lcm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+
+{
+    int num1, num2, n1, n2, rem, gcd, lcm;
+    printf("Enter 2 Numbers:\n");
+    scanf("%d %d", &num1, &num2);
+
+    n1 = num1;
+    n2 = num2;
+
+    while(n2 != 0)
+    {
+        rem = n1 % n2;
+        n1 = n2;
+        n2 = rem;
+    }
+
+    gcd = n1;
+    printf("GCD is %d\n", gcd);
+
+    lcm = (num1 * num2) / gcd;
+    printf("LCM is %d\n", lcm);
+
+    return 0;
+
+}
