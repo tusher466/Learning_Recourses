47d0cb8c86c5d06add9adc5903120f08760216ce
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-09-04 23:17:03 +0600

    Add strong number checker in strong_number.c
    This program checks if a given number is a strong number, which is a number whose sum of the factorials of its digits equals the number itself.

---
+++ C Programming/Anisul Islam/Chapter 3/strong_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+
+{
+    int n, rem, temp, sum = 0;
+    printf("Enter any number:\n");
+    scanf("%d", &n);
+
+    temp = n;
+    while(temp != 0)
+    {
+        rem = temp % 10;
+
+        int fact = 1;
+        for(int i = 1; i <= rem; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        temp /= 10;
+    }
+
+    if(sum == n)
+    {
+        printf("Strong Number\n");
+    }
+    else
+    {
+        printf("Not Strong Number\n");
+    }
+
+    return 0;
+}
